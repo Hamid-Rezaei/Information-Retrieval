eef709bd7be7cbf52eb1f9d848fb5ca686d89743
hamid <hamidrezaei.ce@gmail.com>
2024-01-28 20:47:17 +0330

    feat: before presentation

--- invertedIndex/pinverted_index.py
+++ invertedIndex/pinverted_index.py
@@ -1,3 +1,5 @@
+import pickle
+
 from invertedIndex.vectorspace import calculate_tf_idf
 
 
@@ -9,19 +11,11 @@ class Term:
         self.weight_in_docs = {}
         self.champions_list = {}
 
-    def update_posting(self, doc_id, term_position):
-        if doc_id not in self.position_in_docs:
-            self.position_in_docs[doc_id] = []
-            self.term_freq_in_docs[doc_id] = 0
-        self.position_in_docs[doc_id].append(term_position)
-        self.term_freq_in_docs[doc_id] += 1
-        self.collect_freq += 1
+    def create_champ_list(self, k):
+        self.champions_list = dict(sorted(self.weight_in_docs.items(), key=lambda item: item[1], reverse=True)[:k])
 
-    def get_docs(self):
-        return self.position_in_docs.keys()
-
-    def get_weight_in_doc(self, doc_id):
-        return self.weight_in_docs[doc_id]
+    def get_champ_list(self):
+        return self.champions_list
 
     def calculate_weight(self, doc_id, collection_size):
         self.weight_in_docs[doc_id] = calculate_tf_idf(
@@ -30,8 +24,30 @@ class Term:
             collection_size
         )
 
-    def create_champ_list(self, k):
-        self.champions_list = dict(sorted(self.weight_in_docs.items(), key=lambda item: item[1], reverse=True)[:k])
+    def get_weight_in_doc(self, doc_id):
+        return self.weight_in_docs[doc_id]
 
-    def get_champ_list(self):
-        return self.champions_list
+    def get_docs(self):
+        return self.position_in_docs.keys()
+
+    def update_postings_list(self, doc_id, term_position):
+        if doc_id not in self.position_in_docs:
+            self.position_in_docs[doc_id] = []
+            self.term_freq_in_docs[doc_id] = 0
+        self.position_in_docs[doc_id].append(term_position)
+        self.term_freq_in_docs[doc_id] += 1
+        self.collect_freq += 1
+
+
+class InvertedIndex:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def save(self, path: str):
+        with open(path, 'wb') as file:
+            pickle.dump(self.dictionary, file)
+
+    def load(self, path: str):
+        with open(path, 'wb') as file:
+            dictionary = pickle.load(file)
+        return dictionary
